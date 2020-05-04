5d2c3b90dd2ad109305e6ccd0dce777f0e481d52
VladChechetko <vlad-chechetko@yandex.ru>
2020-05-04 19:36:30 +0300

    docker-compose fix

--- taskcloud-service/src/main/java/ru/school/cloud/taskcloud/service/InfoController.java
+++ taskcloud-service/src/main/java/ru/school/cloud/taskcloud/service/InfoController.java
@@ -1,12 +1,14 @@
 package ru.school.cloud.taskcloud.service;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/info")
+@Lazy
 public class InfoController {
 	
 	@Value("${app.version}")
